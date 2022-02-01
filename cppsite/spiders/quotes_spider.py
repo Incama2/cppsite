545d364d75773c0d602328eed6524e44d441916c
Incama <incamamailbox@gmail.com>
2022-02-01 18:03:48 +0100

    Working with cppsite 2.0

--- cppsite/spiders/quotes_spider.py
+++ cppsite/spiders/quotes_spider.py
@@ -2,19 +2,24 @@ import scrapy
 
 
 class QuotesSpider(scrapy.Spider):
-    name = "quotes"
+    name = "cppsite"
+
+    tab1 = [1,2]
 
     def start_requests(self):
-        urls = [
-            'http://quotes.toscrape.com/page/1/',
-            'http://quotes.toscrape.com/page/2/',
-        ]
-        for url in urls:
+        for i in self.tab1:
+            url = 'https://webs.iiitd.edu.in/raghava/cppsite/browse_sub1.php?token=Linear&col=5&page=%d' % i
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        page = response.url.split("/")[-2]
-        filename = f'quotes-{page}.html'
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-        self.log(f'Saved file {filename}')
+        for row in response.css("tr"):
+            yield {
+                'id': row.css("td:nth-of-type(1) a::text").extract_first(),
+                'sequence': row.css("td:nth-of-type(2) a::text").extract_first(),
+                'pubmedid': row.css("td:nth-of-type(13) a::text").extract_first()
+            }
+
+        next_page_url = response.css(".pagination a:last-of-type::attr(href)").extract_first()
+        if next_page_url is not None:
+            yield scrapy.Request(response.urljoin(next_page_url))
+        
