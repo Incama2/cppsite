403d13e856e16e04a0780959aba4803e08089cad
Incama <incamamailbox@gmail.com>
2022-02-09 19:43:04 +0100

    work

--- cppsite/spiders/quotes_spider.py
+++ cppsite/spiders/quotes_spider.py
@@ -4,22 +4,34 @@ import scrapy
 class QuotesSpider(scrapy.Spider):
     name = "cppsite"
 
-    tab1 = [1,2]
+    # Modify this token if you want to scrape different categories
+    token = "protein"
 
     def start_requests(self):
-        for i in self.tab1:
-            url = 'https://webs.iiitd.edu.in/raghava/cppsite/browse_sub1.php?token=Linear&col=5&page=%d' % i
-            yield scrapy.Request(url=url, callback=self.parse)
+        url = 'https://webs.iiitd.edu.in/raghava/cppsite/browse_sub1.php?token=%s&col=6' % self.token
+        yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
         for row in response.css("tr"):
-            yield {
-                'id': row.css("td:nth-of-type(1) a::text").extract_first(),
-                'sequence': row.css("td:nth-of-type(2) a::text").extract_first(),
-                'pubmedid': row.css("td:nth-of-type(13) a::text").extract_first()
-            }
+            id = row.css("td:nth-of-type(1) a::text").get()
+            if id is not None:
+                yield {
+                    'id': id,
+                    'sequence': row.css("td:nth-of-type(2) a::text").get(),
+                    'name': row.css("td:nth-of-type(3) font::text").get(),
+                    'length': row.css("td:nth-of-type(4) font::text").get(),
+                    'chirality': row.css("td:nth-of-type(5) font::text").get(),
+                    'linear/cyclic': row.css("td:nth-of-type(6) font::text").get(),
+                    'source': row.css("td:nth-of-type(7) font::text").get(),
+                    'category': row.css("td:nth-of-type(8) font::text").get(),
+                    'N_TERMINAL_MODIFICATION': row.css("td:nth-of-type(9) font::text").get(),
+                    'C_TERMINAL_MODIFICATION': row.css("td:nth-of-type(10) font::text").get(),
+                    'CHEMICAL_MODIFICATION': row.css("td:nth-of-type(11) font::text").get(),
+                    'cargo': row.css("td:nth-of-type(12) font::text").get(),
+                    'pubmedid': row.css("td:nth-of-type(13) a::text").get()
+                }
 
-        next_page_url = response.css(".pagination a:last-of-type::attr(href)").extract_first()
+        next_page_url = response.css(".pagination a:last-of-type::attr(href)").get()
         if next_page_url is not None:
             yield scrapy.Request(response.urljoin(next_page_url))
         
